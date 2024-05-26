cde3df34e458252c65e1d25730c7f2a549245af3
Omar Valdez <omarantoniovaldezf2@gmail.com>
2024-05-26 02:31:40 -0700

    feat(views,template): Show recommendations for logged user

--- MyMovies/movies/views.py
+++ MyMovies/movies/views.py
@@ -21,18 +21,22 @@ from .forms import MovieReviewForm
 
 if TYPE_CHECKING:
     from collections.abc import Iterator
-    from typing import Protocol, TypeVar
+    from typing import Protocol, TypeAlias, TypeVar
+
+    from django.contrib.auth.models import AbstractBaseUser, AnonymousUser
+    from django.db.models import QuerySet
 
     from .models import MovieReview
 
     T = TypeVar('T', covariant=True)
 
+    ScoreWithHasReview: TypeAlias = tuple[list[float], list[bool]]
+    RecommendationContext: TypeAlias = tuple[zip[tuple[Movie, float, bool]], bool]
+    AnyUser: TypeAlias = AbstractBaseUser | AnonymousUser
+
     class IterLen(Protocol[T]):
         def __iter__(self) -> Iterator[T]: ...
         def __len__(self) -> int: ...
-
-
-LOGIN_PAGE_URL = 'user-login'
 
 
 def calc_movie_score(reviews: IterLen[MovieReview]) -> float:
@@ -42,17 +46,42 @@ def calc_movie_score(reviews: IterLen[MovieReview]) -> float:
     return sum(r.rating for r in reviews) / length
 
 
-def index(request: HttpRequest):
-    movies = Movie.objects.order_by('-release_date')
+def get_user_recommendations(user: User) -> QuerySet[Movie]:
+    reviewed_movies = Movie.objects.filter(moviereview__user_id=user)
+    unreviewed_movies = Movie.objects.exclude(id__in=reviewed_movies)[:4]
+    return unreviewed_movies
+
+
+def score_with_has_review(movies: QuerySet[Movie]) -> ScoreWithHasReview:
     scores = []
     has_reviews = []
-
     for movie in movies:
         reviews = movie.moviereview_set.all()
         scores.append(calc_movie_score(reviews))
         has_reviews.append(bool(reviews))
+    return scores, has_reviews
 
-    context = {'movies': movies, 'movies_with_score': zip(movies, scores, has_reviews)}
+
+def recommendation_context(user: AnyUser) -> RecommendationContext:
+    if not user.is_authenticated:
+        return zip(), False
+    assert isinstance(user, User)
+    recommendations = get_user_recommendations(user)
+    recomm_with_score = zip(recommendations, *score_with_has_review(recommendations))
+    has_recommendations = bool(recommendations)
+    return recomm_with_score, has_recommendations
+
+
+def index(request: HttpRequest):
+    movies = Movie.objects.order_by('-release_date')
+    recommendations, has_recommendations = recommendation_context(request.user)
+
+    context = {
+        'movies': movies,
+        'movies_with_score': zip(movies, *score_with_has_review(movies)),
+        'recommendations': recommendations,
+        'has_recommendations': has_recommendations,
+    }
     return render(request, 'movies/index.html', context)
 
 
@@ -60,9 +89,16 @@ def user_page(request: HttpRequest, user_id: int) -> HttpResponse:
     try:
         user = User.objects.get(pk=user_id)
     except ObjectDoesNotExist:
-        raise Http404(f'Movie with id {user_id} does not exists')
+        raise Http404(f'User with id {user_id} does not exists')
     reviews = user.moviereview_set.order_by('-date_time')
-    context = {'reviews': reviews, 'page_user': user}
+
+    recommendations, has_recommendations = recommendation_context(request.user)
+    context = {
+        'reviews': reviews,
+        'page_user': user,
+        'recommendations': recommendations,
+        'has_recommendations': has_recommendations,
+    }
     return render(request, 'movies/user_page.html', context)
 
 
@@ -96,7 +132,14 @@ def movie_detail(request: HttpRequest, movie_id: int):
         raise Http404(f'Movie with id {movie_id} does not exists')
     reviews = movie.moviereview_set.order_by('-date_time')
     score = calc_movie_score(reviews)
-    context = {'movie': movie, 'reviews': reviews, 'score': score}
+    recommendations, has_recommendations = recommendation_context(request.user)
+    context = {
+        'movie': movie,
+        'reviews': reviews,
+        'score': score,
+        'recommendations': recommendations,
+        'has_recommendations': has_recommendations,
+    }
     return render(request, 'movies/movie_detail.html', context)
 
 
@@ -108,7 +151,7 @@ def review_form(request: HttpRequest, movie_id: int) -> HttpResponse:
     return render(request, 'movies/review_form.html', {'movie': movie})
 
 
-@login_required(login_url=LOGIN_PAGE_URL)
+@login_required(login_url='user-login')
 def submit_movie_review(request: HttpRequest, movie_id: int):
     if request.method != 'POST':
         return HttpResponseNotAllowed('GET')
